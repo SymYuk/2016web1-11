d36178f442a7c3e9500ad8f282c65ccac3c6da54
SymYuk <g1527043@st.t-kougei.ac.jp>
2016-06-20 17:19:29 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,29 +1,32 @@
 var num=Math.floor(Math.random()*256+1);
 var chance=8;
 var wordC="●●●●●●●●"
-var word="1~256の数あてデス！\nチャンス : "+wordC
+var word="1~256の数あてデス！\nチャンス : "+wordC;
 var input=parseInt(window.prompt(word));
 
 var message;
 
 while(true)
 {
-if(answer===number)
-message="正解！！";
-else
-{
-  chance--;
-  wordC.replace("●","");
-  if(chance==0)
-  {
-  message="ゲームオーバー！\n正解は"+num+"です";
-document.getElementById('choice').textContent=message;
-break;
-  }
-  else if(input<num)
-  message="もっと大きいよ！チャンス : "+wordC
-  else if(input>num)
-  message="もっと小さいよ！チャンス : "+wordC
-}
+	if(answer===number)
+	{
+		message="正解！！";
+		document.getElementById('choice').textContent=message;
+	}
+	else
+	{
+  		chance--;
+  		wordC.replace("●","");
+  		if(chance==0)
+  		{
+  			message="ゲームオーバー！\n正解は"+num+"です";
+			document.getElementById('choice').textContent=message;
+			break;
+  		}
+  		else if(input<num)
+  		message="もっと大きいよ！チャンス : "+wordC
+  		else if(input>num)
+  		message="もっと小さいよ！チャンス : "+wordC
+	}
 document.getElementById('choice').textContent=message;
 }
