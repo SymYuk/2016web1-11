817745d7b1c6fbd08f15eebed759e841990f4403
SymYuk <g1527043@st.t-kougei.ac.jp>
2016-06-20 18:12:26 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 var num=Math.floor(Math.random()*256+1);
 var chance=8;
-var wordC="●●●●●●●●";
-var word='1~256の数あてデス！\nチャンス : '+wordC;
+var word='1~256の数あてデス！\nチャンス : '+chance+'回';
 var input;
 var message;
 
@@ -11,24 +10,21 @@ while(true)
 	if(input===num)
 	{
 		message='正解！おめでとう！！';
-	//	break;
 	}
 	else
 	{
   		chance-=1;
-  		//wordC=wordC.substr(0,text.length-1);
   		if(chance===0)
   		{
   			message='ゲームオーバー！\n正解は'+num+'です';
-		//	break;
   		}
   		else if(input<num)
   		{
-  			message='もっと大きいよ！\nチャンス : '+wordC;
+  			message='もっと大きいよ！;
   		}
   		else if(input>num)
   		{
-  			message='もっと小さいよ！\nチャンス : '+wordC;
+  			message='もっと小さいよ！;
 		}	
 		else{}
 	}
